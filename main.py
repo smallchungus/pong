62ec330a3de5c2286185413f974fef36766288b0
WillMACOS <wchen1396@gmail.com>
2023-04-30 16:07:36 -0400

    adding paddle a

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import turtle
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Create a turtle
+window = turtle.Screen()
+window.title("Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
 
+while True:
+    window.update()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
