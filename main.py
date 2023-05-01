39ca0f1ba15f87fe9ec70e92782e427b3a7c43c3
WillMACOS <wchen1396@gmail.com>
2023-05-01 09:07:47 -0400

    pong game commit to paddles

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
-while True:
-    window.update()
-
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=10, stretch_len=1)
 paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle b
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=10, streth_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# ball
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(0, 0)
+
+#Main game loop
+while True:
+    window.update()
